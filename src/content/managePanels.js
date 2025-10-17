84a0960d9b8d19855c311214896976999604578d
bernardo-pernes <bernardo.souza@petrus-software.com>
2025-10-17 15:52:18 -0300

    fix: adpanel removal logic

--- src/content/managePanels.js
+++ src/content/managePanels.js
@@ -1,50 +1,60 @@
 function handlePanelToggle(radioPanelStatus) {
-  const radioPanel = document.querySelector('#area_player')
-  const value = radioPanelStatus ? 'hidden' : ''
-  radioPanel.setAttribute('style', `content-visibility: ${value};`)
+	const radioPanel = document.querySelector('#area_player')
+	const value = radioPanelStatus ? 'hidden' : ''
+	radioPanel.setAttribute('style', `content-visibility: ${value};`)
 }
 
 function handlePlayerToggle(radioPlayerStatus) {
-  const radioPlayer = document.querySelector('#player2')
-  if (radioPlayerStatus) {
-    radioPlayer.removeAttribute('autoplay')
-    radioPlayer.pause()
-  } else {
-    radioPlayer.play()
-  }
+	const radioPlayer = document.querySelector('#player2')
+	if (radioPlayerStatus) {
+		radioPlayer.removeAttribute('autoplay')
+		radioPlayer.pause()
+	} else {
+		radioPlayer.play()
+	}
 }
 
 async function handleInitialValues() {
-  const result = await chrome.storage.sync.get([
-    'radioPanelStatus',
-    'radioPlayerStatus',
-  ])
-  const parsedStatus = JSON.parse(
-    `{"radioPanelStatus": ${result.radioPanelStatus}, "radioPlayerStatus": ${result.radioPlayerStatus}}`
-  )
+	const result = await chrome.storage.sync.get([
+		'radioPanelStatus',
+		'radioPlayerStatus',
+	])
+	const parsedStatus = JSON.parse(
+		`{"radioPanelStatus": ${result.radioPanelStatus}, "radioPlayerStatus": ${result.radioPlayerStatus}}`
+	)
 
-  const { radioPanelStatus, radioPlayerStatus } = parsedStatus
-  handlePanelToggle(radioPanelStatus)
-  handlePlayerToggle(radioPlayerStatus)
+	const { radioPanelStatus, radioPlayerStatus } = parsedStatus
+	handlePanelToggle(radioPanelStatus)
+	handlePlayerToggle(radioPlayerStatus)
 }
 
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  if (request.action === 'first-load-or-refresh') {
-    const adPanels = document.querySelectorAll('[id="675hfgh3453"], [id="6fs7567545g"]')
-    const googlePanels = document.querySelectorAll(
-      '.adsbygoogle, .google_ad, [id^="google_ads"], [class*="adsbygoogle"]'
-    )
-    const iframes = document.querySelectorAll('iframe')
+	if (request.action === 'first-load-or-refresh') {
+		const xpath = "/html/body/div[1]"
+		const evaluator = new XPathEvaluator()
+		const expression = evaluator.createExpression(xpath)
+		const result = expression.evaluate(document, XPathResult.FIRST_ORDERED_NODE_TYPE)
 
-    removeDomElements([iframes, adPanels, googlePanels])
-    handleInitialValues()
+		const adPanels = document.querySelectorAll('[id="675hfgh3453"], [id="6fs7567545g"]')
+		const googlePanels = document.querySelectorAll(
+			'.adsbygoogle, .google_ad, [id^="google_ads"], [class*="adsbygoogle"]'
+		)
+		const iframes = document.querySelectorAll('iframe')
 
-    sendResponse({ status: 'Executado' })
-  } else if (request.action === 'hide-radio-panel') {
-    const { radioPanelStatus } = request
-    handlePanelToggle(radioPanelStatus)
-  } else if (request.action === 'radio-player-click') {
-    const { radioPlayerStatus } = request
-    handlePlayerToggle(radioPlayerStatus)
-  }
+		removeDomElements([iframes, adPanels, googlePanels])
+
+		const adPanelAncestor = result.singleNodeValue
+		if (adPanelAncestor) {
+			adPanelAncestor.remove()
+		}
+		handleInitialValues()
+
+		sendResponse({ status: 'Executado' })
+	} else if (request.action === 'hide-radio-panel') {
+		const { radioPanelStatus } = request
+		handlePanelToggle(radioPanelStatus)
+	} else if (request.action === 'radio-player-click') {
+		const { radioPlayerStatus } = request
+		handlePlayerToggle(radioPlayerStatus)
+	}
 })
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,52 +1,50 @@
 import { retrieveSyncStorageValue } from '../utils/storage.js'
 
-console.log('LOADEDDD!!')
-
 const radioPanelCheckbox = document.querySelector("input[name='hide-player']")
 const stopRadioElement = document.querySelector("input[name='stop-radio']")
 
 radioPanelCheckbox.addEventListener('click', async () => {
-  const radioState = radioPanelCheckbox.checked
-  const value = radioState.toString()
-  await chrome.storage.sync.set({ radioPanelState: value })
+	const radioState = radioPanelCheckbox.checked
+	const value = radioState.toString()
+	await chrome.storage.sync.set({ radioPanelState: value })
 
-  try {
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      lastFocusedWindow: true,
-    })
-    await chrome.tabs.sendMessage(tab.id, {
-      action: 'hide-radio-panel',
-      radioPanelStatus: radioState,
-    })
-  } catch (e) {
-    console.error(e)
-  }
+	try {
+		const [tab] = await chrome.tabs.query({
+			active: true,
+			lastFocusedWindow: true,
+		})
+		await chrome.tabs.sendMessage(tab.id, {
+			action: 'hide-radio-panel',
+			radioPanelStatus: radioState,
+		})
+	} catch (e) {
+		console.error(e)
+	}
 })
 
 stopRadioElement.addEventListener('click', async () => {
-  const radioPlayerStatus = stopRadioElement.checked
-  const value = radioPlayerStatus.toString()
-  await chrome.storage.sync.set({ radioPlayerStatus: value })
+	const radioPlayerStatus = stopRadioElement.checked
+	const value = radioPlayerStatus.toString()
+	await chrome.storage.sync.set({ radioPlayerStatus: value })
 
-  try {
-    const [tab] = await chrome.tabs.query({
-      active: true,
-      lastFocusedWindow: true,
-    })
-    await chrome.tabs.sendMessage(tab.id, {
-      action: 'radio-player-click',
-      radioPlayerStatus: radioPlayerStatus,
-    })
-  } catch (e) {
-    console.error(e)
-  }
+	try {
+		const [tab] = await chrome.tabs.query({
+			active: true,
+			lastFocusedWindow: true,
+		})
+		await chrome.tabs.sendMessage(tab.id, {
+			action: 'radio-player-click',
+			radioPlayerStatus: radioPlayerStatus,
+		})
+	} catch (e) {
+		console.error(e)
+	}
 })
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const newPanelState = await retrieveSyncStorageValue('radioPanelStatus')
-  const newPlayerState = await retrieveSyncStorageValue('radioPlayerStatus')
+	const newPanelState = await retrieveSyncStorageValue('radioPanelStatus')
+	const newPlayerState = await retrieveSyncStorageValue('radioPlayerStatus')
 
-  radioPanelCheckbox.checked = newPanelState
-  stopRadioElement.checked = newPlayerState
+	radioPanelCheckbox.checked = newPanelState
+	stopRadioElement.checked = newPlayerState
 })
